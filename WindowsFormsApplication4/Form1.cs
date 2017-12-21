36dd5252bb40e02412c2ba745f5d538662f1ff34
Ivan <john.matyash@gmail.com>
2017-12-21 19:34:38 +0200

    non-default mesh folder support

--- WindowsFormsApplication4/Form1.cs
+++ WindowsFormsApplication4/Form1.cs
@@ -11,6 +11,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Win32;
 
 namespace WindowsFormsApplication4
 {
@@ -24,6 +25,8 @@ namespace WindowsFormsApplication4
 
         String folderPath = null;
         String altiumID = null;
+        String productType = null;
+        String meshFolder = null;
 
         String warningFolderMissing = "Please select correct Altium folder";
 
@@ -98,10 +101,12 @@ namespace WindowsFormsApplication4
                 //MessageBox.Show(res);
                 string final_res = res.Trim();
                 richTextBox1.Text += "Product type: " + final_res + "\n";
+                productType = final_res;
                 return final_res;
             }
             else
             {
+                productType = res;
                 return res;
             }
             
@@ -109,7 +114,10 @@ namespace WindowsFormsApplication4
 
         private void richTextBox1_TextChanged(object sender, EventArgs e)
         {
-
+            // set the current caret position to the end
+            richTextBox1.SelectionStart = richTextBox1.Text.Length;
+            // scroll it automatically
+            richTextBox1.ScrollToCaret();
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -144,6 +152,7 @@ namespace WindowsFormsApplication4
 
         private void deleteInFolder(string filePath)
         {            
+
             if (Directory.Exists(filePath))
             {
                 richTextBox1.Text += "Starting the deletion of files at " + filePath + "\n";
@@ -163,19 +172,62 @@ namespace WindowsFormsApplication4
                 MessageBox.Show("There is no Mesh folder");            
         }
 
+        private string searchRegistry()
+        {
+            string res = null;
+            try
+            {
+                using (RegistryKey key = Registry.CurrentUser.OpenSubKey("Software\\Altium\\" + findProductType(folderPath) + " {" + findAltiumID(folderPath) + "}\\DesignExplorer\\Preferences\\AdvPCB\\TModelOptions"))
+                {
+                    if (key != null)
+                    {
+                        Object o = key.GetValue("MeshDirectory");
+                        if (o != null)
+                        {
+                            //Version version = new Version(o as String);  //"as" because it's REG_SZ...otherwise ToString() might be safe(r)
+                            meshFolder = o.ToString();
+                            return o.ToString();                                             //do what you like with version
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)  //just for demonstration...it's always best to handle specific exceptions
+            {
+                //react appropriately
+            }
+            return res;
+        }
         private void button5_Click(object sender, EventArgs e)
         {
-            WindowsIdentity user = WindowsIdentity.GetCurrent();
-            string userName = user.Name.Substring(user.Name.LastIndexOf("\\") + 1);
+            DialogResult dialogResult = MessageBox.Show("Are you sure to delete Mesh folder", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dialogResult == DialogResult.Yes)
+            {
+                //do something
+                WindowsIdentity user = WindowsIdentity.GetCurrent();
+                string userName = user.Name.Substring(user.Name.LastIndexOf("\\") + 1);
 
-            if (folderPath == null)
-            {
-                MessageBox.Show(warningFolderMissing);
+                if (folderPath == null)
+                {
+                    MessageBox.Show(warningFolderMissing);
+                }
+                else
+                {
+                    if (searchRegistry() == "%AltiumMeshApplicationData%")
+                    {
+                        deleteInFolder(@"C:\Users\" + userName + @"\AppData\Local\Altium\" + productType + " {" + findAltiumID(folderPath) + @"}\Mesh");
+                    }
+                    else
+                    {
+                        deleteInFolder(meshFolder);
+                    }
+                }
             }
-            else
+            else if (dialogResult == DialogResult.No)
             {
-                deleteInFolder(@"C:\Users\" + userName + @"\AppData\Local\Altium\" + findProductType(folderPath) + " {" + findAltiumID(folderPath) + @"}\Mesh");
+                //do something else
             }
+
+            
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -209,6 +261,31 @@ namespace WindowsFormsApplication4
             {
                 MessageBox.Show(warningFolderMissing);
             }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            var clickPos = this.PointToClient(new System.Drawing.Point(MousePosition.X, MousePosition.Y));
+
+            // If click is over the right-hand portion of the button show the menu
+            if (clickPos.X >= (Size.Width - 10))
+                ShowMenuUnderControl();
+            else
+                base.OnClick(e);
+        }
+        // If you want right-mouse click to invoke the menu override the mouse up event
+        protected override void OnMouseUp(MouseEventArgs mevent)
+        {
+            if ((mevent.Button & MouseButtons.Right) != 0)
+                ShowMenuUnderControl();
+            else
+                base.OnMouseUp(mevent);
+        }
+
+        // Raise the context menu
+        public void ShowMenuUnderControl()
+        {
+            //splitMenuStrip.Show(this, new Point(0, this.Height), ToolStripDropDownDirection.BelowRight);
         }
     }
 }
